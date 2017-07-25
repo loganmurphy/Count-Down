915cf29d4781b5a12b0099409ef8decc1e25bd4f
loganmurphy <loganmurphy1984@gmail.com>
2017-07-24 21:53:03 -0500

    Update countdownV_1.js

--- countdownV_1.js
+++ countdownV_1.js
@@ -1,12 +1,3 @@
-//This fucntion gets the user's date and saves it into a variable as a string.
-  function getDate() {
-    var monthAndDay = document.getElementById("target-month-day").value;
-    var year = document.getElementById("target-year").value;
-    var targetDate = monthAndDay + " " + year;
-    var dateString = targetDate.toString();
-    console.log(dateString);
-  }
-
 // This funciton takes the user's date input and subtracts the current date value to reach a "days til" value.
   function countDown() {
     var monthAndDay = document.getElementById("target-month-day").value;
